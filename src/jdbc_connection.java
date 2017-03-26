224c5dcab2563e4c200f1b5a149b2fad805ea1b1
Vivek Bhat <vivekbhat@live.com>
2017-03-25 21:30:27 -0400

    jdbc_connection.java added

---
+++ src/jdbc_connection.java
@@ -0,0 +1,9 @@
+import java.sql.*;
+
+public class jdbc_connection {
+	public static void main(String[] args) {
+//		System.out.println(System.getenv("JDBC_NCSU_UN"));
+		// System.out.println("Vivek");
+	}
+
+}
