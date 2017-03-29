4d014b2fe86fce2d763767a2933a54065404c7ef
Vivek Bhat <vivekbhat@live.com>
2017-03-28 21:34:43 -0400

    JDBC Connection file added

--- src/jdbc_connection.java
+++ src/jdbc_connection.java
@@ -1,9 +1,57 @@
-import java.sql.*;
 
-public class jdbc_connection {
-	public static void main(String[] args) {
-//		System.out.println(System.getenv("JDBC_NCSU_UN"));
-		// System.out.println("Vivek");
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Vivek Bhat
+ */
+public class JDBC_Connection {
+	
+	Creds obj = new Creds();
+	
+	static final String jdbcURL = "jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01";
+	private String user_name = obj.userName;
+	private String password = obj.password;
+
+	private JDBC_Connection() {
+
+	}
+
+	public Connection makeConnection() {
+		Connection conn = null;
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			try {
+
+				conn = DriverManager.getConnection(jdbcURL, this.user_name, this.password);
+
+			} finally {
+
+				close(conn);
+			}
+		} catch (Throwable oops) {
+			oops.printStackTrace();
+		}
+
+		return conn;
+	}
+
+	static void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Throwable whatever) {
+			}
+		}
 	}
 
 }
